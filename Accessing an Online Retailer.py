bff6ccba2e9870e4e3c0c1c61d7fb3c80bf5aa57
Deadlymind <deadlygamex5@gmail.com>
2024-06-01 11:17:04 +0100

    feat: Add Selenium script for automating login to Titan22
    - Implement get_driver function to set up Chrome WebDriver with appropriate options for easier browsing
    - Configure WebDriver to disable infobars, maximize window, disable dev-shm-usage, and exclude automation switches
    - Use Service to specify the path to the ChromeDriver executable
    - Implement main function to automate login process on Titan22 website
    - Perform actions to enter email and password, submit login form, and navigate to a specific page
    - Add delays using time.sleep to ensure elements are loaded before interacting
    
    This script provides a basic implementation for automating login and navigation tasks using Selenium WebDriver.

---
+++ Accessing an Online Retailer.py
@@ -0,0 +1,35 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.keys import Keys
+import time
+
+def get_driver():
+    # set options to make browsing easier
+    options = webdriver.ChromeOptions()
+    options.add_argument('disable-infobars')
+    options.add_argument('start-maximized')
+    options.add_argument('disable-dev-shm-usage')
+    options.add_argument('no-sandbox')
+    options.add_experimental_option('excludeSwitches', ['enable-automation'])
+    options.add_argument('disable-blink-features=AutomationControlled')
+    options.add_experimental_option('excludeSwitches', ['enable-logging'])
+
+    service = Service(r'C:\Users\deadl\Desktop\SMP\Automate\src\chromedriver.exe')
+    driver = webdriver.Chrome(service=service, options=options)
+    driver.get("https://titan22.com/account/login?return_url=%2Faccount")
+
+    return driver
+
+def main():
+    driver = get_driver()
+    driver.find_element(By.ID, value="CustomerEmail").send_keys("oussamaayari2014@gmail.com")
+    time.sleep(2)
+    driver.find_element(By.ID, value="CustomerPassword").send_keys("v_$dFK72$H*bVBS" + Keys.RETURN)
+    time.sleep(2)
+    driver.find_element(By.XPATH, value="/html/body/footer/div/section/div/div[1]/div[1]/div[1]/nav/ul/li[1]/a").click()
+    time.sleep(3)
+    print(driver.current_url)
+
+if __name__ == "__main__":
+    print(main())
